09d59c19656a00a96954b0d385d461e6f8529d72
Краснова Татьяна <redtanya@yandex.ru>
2022-05-17 15:07:50 +0300

    dorabotka

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,19 @@
 public class Main {
-        public static void main(String[] args) {
-            int schet = 100;
-            int oplata = 1100;
+    public static void main(String[] args) {
+        int schet = 100;
+        int oplata = 1100;
 
-            int bonus;
-            if (oplata >= 1000) {
-                bonus = oplata/100;
-            } else {
-                bonus = 0;
-            }
-            int itogo = schet+oplata+bonus;
-            System.out.println(itogo);
-            System.out.println(bonus);
-
-
-            // Объявляете переменные для входных данных и
-            // параметров программы: начального счёта,
-            // суммы пополнения и тп
-
-            // Условным оператором проверяете превысила ли
-            // сумма пополнения порог и для этих двух разных
-            // сценариев рассчитываете сумму бонуса и выводите
-            // на экран.
+        int bonus;
+        if (oplata >= 1000) {
+            bonus = oplata / 100;
+        } else {
+            bonus = 0;
         }
+        int itogo = schet + oplata + bonus;
+        System.out.println(itogo);
+        System.out.println(bonus);
+
+
     }
+}
 
